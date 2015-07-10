57388f7bb5bf49346401132490cc59b3d89fbd31
Diego Rocha <diego.rocha@ag2nurun.com.br>
2015-07-09 22:15:03 -0300

    Adding initial structure and material design assets

---
+++ index.js
@@ -0,0 +1,22 @@
+var port = 3000;
+var express = require('express');
+var app = express();
+var path = require('path');
+var http = require('http').Server(app);
+
+app.set('port', port);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+	res.render('index', {});
+});
+
+http.listen(port, function(){
+	console.log('Listening on port: ' + port);
+});
+
+module.exports = app;
